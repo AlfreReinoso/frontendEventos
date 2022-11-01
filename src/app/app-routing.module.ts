a9904a5fa14d00cdc2dc4258e8b46b2b8c50d847
JuaniBenedetti <juanigbenedetti@gmail.com>
2022-10-31 23:56:15 -0300

    Carga de servicios mediante componente servicios-form. Tengo que completar el componente servicios todavía para devolverle la funcionalidad a state.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SalasComponent} from "./Components/salas/salas.component";
-import {SalonComponentComponent} from "./Components/salon-component/salon-component.component";
-import {NavbarComponent} from "./Components/navbar/navbar.component";
-import {ServiciosComponent} from "./servicios/servicios.component";
-import {ServicioComponentComponent} from "./servicio-component/servicio-component.component";
+import { SalasComponent } from "./Components/salas/salas.component";
+import { SalonComponentComponent } from "./Components/salon-component/salon-component.component";
+import { ServiciosFormComponent } from './Components/servicios/servicios-form/servicios-form.component';
+import { ServiciosComponent } from './Components/servicios/servicios.component';
 
 const routes: Routes = [
   {path:'salas', component: SalasComponent},
   {path:'salon/:id',component:SalonComponentComponent},
   {path:'servicios/:id',component:ServiciosComponent},
-  {path:'servicio/:id_salon',component:ServicioComponentComponent}
+  {path:'serviciosForm/:id', component:ServiciosFormComponent}
 ];
 
 @NgModule({
