b4dfabbd78706bbf1e5d6e1860904c3ac6ce81a7
areinoso <areinoso@amr.org.ar>
2023-08-04 10:13:14 -0300

    agregamos mas tiempo para el menu

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -68,7 +68,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   async setearMenu(event:any){
-    await new Promise(r => setTimeout(r, 1000));
+    await new Promise(r => setTimeout(r, 1500));
     if(event == true){
       this.isUserLoggedIn = true
       this.cliente = this.store.selectSnapshot(ClienteState.getCliente);
